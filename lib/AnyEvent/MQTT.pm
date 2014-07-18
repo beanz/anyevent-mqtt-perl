d7d5df2e1fd8cb1c6b53a6abdecc7ef61aa519c8
Mark Hindess <soft-github@temporalanomaly.com>
2014-07-18 14:37:08 +0100

    Build results of fb843a3 (on master)

--- lib/AnyEvent/MQTT.pm
+++ lib/AnyEvent/MQTT.pm
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 package AnyEvent::MQTT;
-$AnyEvent::MQTT::VERSION = '1.141991';
+$AnyEvent::MQTT::VERSION = '1.141992';
 # ABSTRACT: AnyEvent module for an MQTT client
 
 
@@ -644,7 +644,7 @@ AnyEvent::MQTT - AnyEvent module for an MQTT client
 
 =head1 VERSION
 
-version 1.141991
+version 1.141992
 
 =head1 SYNOPSIS
 
